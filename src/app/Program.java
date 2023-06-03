077b291aa66e1a87155487471d822fe2cd279373
guifi <guifi@DESKTOP-LHDEIMI>
2023-06-03 17:50:53 -0300

    metodo de saque e deposito adicionado

--- src/app/Program.java
+++ src/app/Program.java
@@ -3,9 +3,49 @@ package app;
 public class Program {
 
 	public static void main(String[] args) {
-
-		System.out.println("Hello World");
-
+		// COLUNAS
+		// id, nome, cpf, email, senha, dinheiro
+		String[][] bancoDeDados = new String[15][6];
+		String[][] db = {
+				{"0", "pedro", "019131", "gmail", "qq115", "100.0"},
+				{"1", "pedro", "019131", "gmail", "qq115", "200.0"}
+		};
+		
+		
+	}
+	public static void sacarDinheiro(double valor, int id, String[][] db) {
+		if (valor <= 0) {
+			System.out.println("Digite um valor válido");
+			return;
+		}
+		int count = 0;
+		
+		while(count <= db.length) {
+			if(Integer.parseInt(db[count][0]) == id) {
+				double temp = Double.parseDouble(db[count][5]) - valor;
+				db[count][5] = temp + "";
+				return;
+			}
+			count++;
+		}
+		System.out.println("Id inválido");
+	}
+	public static void depositarDinheiro(double valor, int id, String[][] db) {
+		if (valor <= 0) {
+			System.out.println("Digite um valor válido");
+			return;
+		}
+		int count = 0;
+		
+		while(count <= db.length) {
+			if(Integer.parseInt(db[count][0]) == id) {
+				double temp = Double.parseDouble(db[count][5]) + valor;
+				db[count][5] = temp + "";
+				return;
+			}
+			count++;
+		}
+		System.out.println("Id inválido");
 	}
 
 }
