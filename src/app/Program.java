31df1fc5b4f1d21276f4cbb0f9b3b301d1b5cd96
guifi <guifi@DESKTOP-LHDEIMI>
2023-06-03 19:39:31 -0300

    CRUD feito

--- src/app/Program.java
+++ src/app/Program.java
@@ -3,16 +3,101 @@ package app;
 public class Program {
 
 	public static void main(String[] args) {
+		//db conta
 		// COLUNAS
-		// id, nome, cpf, email, senha, dinheiro
-		String[][] bancoDeDados = new String[15][6];
+		// id, nome, cpf, email, senha
+		String[][] contas = new String[15][6];
 		String[][] db = {
-				{"0", "pedro", "019131", "gmail", "qq115", "100.0"},
-				{"1", "pedro", "019131", "gmail", "qq115", "200.0"}
+				{"0", "pedro", "019131", "gmail", "qq115"},
+				{"1", "pedro", "019131", "gmail", "qq115"}
 		};
+		// db banco
+		// COLUNAS
+				// id, nome, cpf, email, senha, saldo
+				String[][] contasBanco = new String[15][6];
+				String[][] db2 = {
+						{"0", "pedro", "019131", "gmail", "qq115", "100.0"},
+						{"1", "pedro", "019131", "gmail", "qq115", "200.0"}
+				};
 		
 		
 	}
+	// CRUD usuario
+	public static void criarUsuario(String nome, String cpf, String email, String senha, String[][] db) {		
+		for(int i = 0; i <= db.length; i++) {
+			if(db[i][0] == "") {
+				db[i][0] = i+"";//id auto-incrementavel;
+				db[i][1] = nome;
+				db[i][2] = cpf;
+				db[i][3] = email;
+				db[i][4] = senha;
+			}
+		}
+	}
+	public static String lerUsuario(int id, String[][] db) {
+		return "Nome: " + db[id][1] + "\nEmail: " + db[id][3] + "\nCpf: " + db[id][2];
+	}
+	public static void editarUsuario(int id,String nome, String cpf, String email, String senha, String[][] db) {		
+		for(int i = 0; i <= db.length; i++) {
+			if(Integer.parseInt(db[i][0]) == id) {
+				db[i][1] = nome;
+				db[i][2] = cpf;
+				db[i][3] = email;
+				db[i][4] = senha;
+			}
+		}
+	}
+	public static void deletarUsuario(int id, String[][] db) {
+		for(int i = 0; i <= db.length; i++) {
+			if(Integer.parseInt(db[i][0]) == id) {
+				db[i][0] = "";
+				db[i][1] = "";
+				db[i][2] = "";
+				db[i][3] = "";
+				db[i][4] = "";
+			}
+		}
+	}
+	
+	// CRUD conta Bancaria
+	public static void criarContaBanco(String nome, String cpf, String email, String senha, String[][] db) {		
+		for(int i = 0; i <= db.length; i++) {
+			if(db[i][0] == "") {
+				db[i][0] = i+"";//id auto-incrementavel;
+				db[i][1] = nome;
+				db[i][2] = cpf;
+				db[i][3] = email;
+				db[i][4] = senha;
+				db[i][5] = 0 + "";
+			}
+		}
+	}
+	public static String lerContaBanco(int id, String[][] db) {
+		return "Nome: " + db[id][1] + "\nEmail: " + db[id][3] + "\nCpf: " + db[id][2] + "\nSaldo: " + db[id][5];
+	}
+	public static void editarContaBanco(int id,String nome, String cpf, String email, String senha, String[][] db) {		
+		for(int i = 0; i <= db.length; i++) {
+			if(Integer.parseInt(db[i][0]) == id) {
+				db[i][1] = nome;
+				db[i][2] = cpf;
+				db[i][3] = email;
+				db[i][4] = senha;
+			}
+		}
+	}
+	public static void deletarContaBanco(int id, String[][] db) {
+		for(int i = 0; i <= db.length; i++) {
+			if(Integer.parseInt(db[i][0]) == id) {
+				db[i][0] = "";
+				db[i][1] = "";
+				db[i][2] = "";
+				db[i][3] = "";
+				db[i][4] = "";
+				db[i][5] = "";
+			}
+		}
+	}
+	//----------------------#--------------------#----------------------------
 	public static void sacarDinheiro(double valor, int id, String[][] db) {
 		if (valor <= 0) {
 			System.out.println("Digite um valor válido");
@@ -80,4 +165,3 @@ public class Program {
 
 
 }
-
